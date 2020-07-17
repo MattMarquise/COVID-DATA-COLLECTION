14f37bacf4143505c70b6f3f06d3fccb1cd870f1
Matthew Marquise <developmatt02@gmail.com>
2020-07-16 22:44:39 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-    $conn=mysqli_connect("localhost","Matthew","Matt2702","coviddatacollection");
+    $conn=mysqli_connect("****","****","****","coviddatacollection");
     // Check connection
     if (mysqli_connect_errno())
     {
